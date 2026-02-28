3c36d28d04e80d316711a0b637af1ee625f90bd0
mitsumakinjo <yagijru@gmail.com>
2026-02-28 19:27:23 +0900

    Add .gitignore file

---
+++ src/main/java/com/mitsuma/training/TrainingApplication.java
@@ -0,0 +1,14 @@
+package com.mitsuma.training;
+
+import org.hibernate.boot.jaxb.mapping.spi.JaxbAssociationAttribute;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApplication.class, args);
+	}
+
+}
